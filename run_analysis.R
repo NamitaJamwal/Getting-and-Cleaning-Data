b41a66f51888f0f3a2bc700449a44259db13213c
Henry <henry.krupp@gmail.com>
2014-08-23 01:29:25 +0900

    comments added

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,11 @@
-# Course Project assignment: create one R script called run_analysis.R that does the following. 
+# Course Project assignment: create one R script called run_analysis.R that does the following.
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+# Source: https://class.coursera.org/getdata-006/human_grading/index
+
 # © Heinrich Krupp 2014 All Rights reserved.
 #
 # global data frames: Data, features
